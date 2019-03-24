05e3cdd664931f403b979c5ce302eb4e309b4ed7
GilTorch <infotorch2014@gmail.com>
2019-03-24 07:17:51 -0400

    Done.

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -4,7 +4,19 @@ import { movies } from '../data';
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+      <h1>Movies Page</h1>
+      <ul>
+    { movies.map((movie)=>{
+          return(
+            <div>
+              <h3>{ movie.title }</h3>
+              <p>Time: { movie.time }</p>
+              <p>Genres: { movie.genres.join(", ") }</p>
+              <p>Metascore: { movie.metascore }</p>
+            </div>
+          )
+     })}
+    </ul>
     </div>
   );
 };
