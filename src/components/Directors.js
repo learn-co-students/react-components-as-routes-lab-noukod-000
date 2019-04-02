ded4e3f079fc46499ca181a0ee22bd8b1205a7b4
dowpm <mcdowpm@gmail.com>
2019-04-02 17:14:36 -0400

    Done.

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -4,7 +4,13 @@ import { directors } from '../data';
 const Directors = () => {
   return (
     <div>
-      {/*{code here}*/}
+        <h1>Directors Page</h1>
+        {directors.map(director =>
+          <div key={director.name}>
+            <h1>Name: {director.name}</h1>
+            <p>Movies:</p>
+            <ul>{director.movies.map(movie => <li key={movie}>{movie}</li>)}</ul>
+          </div>)}
     </div>
   );
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 const Home = () => {
   return (
-    <div>
-      {/*{code here}*/}
-    </div>
+    <React.Fragment>
+      <h1>Home Page</h1>
+    </React.Fragment>
   );
 };
 
