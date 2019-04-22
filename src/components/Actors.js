b5212970c678e385f2934693f6a9d5090b6eb206
Keren-Happuc Fleurimond <keren@noukod.com>
2019-04-22 14:01:19 +0000

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -3,8 +3,20 @@ import { actors } from '../data';
 
 const Actors = () => {
   return (
-    <div>
+    <div class="actors">
       {/*{code here}*/}
+      <h1>Actors Page</h1>
+      {actors.map((actor, index) => (
+        <div key={index}>
+          <h3>{actor.name}</h3>
+          <p>Actors</p>
+          <ul>
+            {actor.movies.map((movie, index) => (
+              <li key={index}>{movie}</li>
+            ))}
+          </ul>
+        </div>
+      ))}
     </div>
   );
 };
